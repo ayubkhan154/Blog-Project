619fdbc71daa5f882dd535ba1d970d7cf5bacb8b
Akshit Tripathi <tripathisoft@gmail.com>
2021-10-30 18:22:30 -0400

    Revert login to use email

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,12 +12,12 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="user_name" class="col-md-4 col-form-label text-md-right">Username</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">E-mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="user_name" type="username" class="form-control @error('email') is-invalid @enderror" name="user_name" value="{{ old('user_name') }}" required autocomplete="username" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                @error('user_name')
+                                @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
