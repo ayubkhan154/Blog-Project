5aff4e2bff8f4245113db1ef0bbe65cd68bdb562
Akshit Tripathi <tripathisoft@gmail.com>
2021-10-30 17:37:55 -0400

    Change 'username' to 'user_name' in login.blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,12 +12,12 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
+                            <label for="user_name" class="col-md-4 col-form-label text-md-right">Username</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="username" class="form-control @error('email') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+                                <input id="user_name" type="username" class="form-control @error('email') is-invalid @enderror" name="user_name" value="{{ old('user_name') }}" required autocomplete="username" autofocus>
 
-                                @error('username')
+                                @error('user_name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
