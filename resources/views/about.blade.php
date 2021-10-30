622ddc6aeb016567249e938dc28c8cf2310c6254
Akshit Tripathi <tripathisoft@gmail.com>
2021-10-30 17:33:06 -0400

    Add about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title')
+    {{$title}}
+@endsection
+@section('content')
+    <section>
+        <h1>About Us</h1>
+        <p>Email: abc@xyz.com</p>
+    </section>
+@endsection
