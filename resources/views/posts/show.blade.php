3dd4b207ba48998825dd6b2e530dbe508f07d7d8
Akshit Tripathi <tripathisoft@gmail.com>
2021-10-30 18:31:39 -0400

    Update posts page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,23 +1,26 @@
 @extends('layouts.app')
 @section('title')
-  @if($post)
-    {{ $post->title }}
-    @if(!Auth::guest() && ($post->author_id == Auth::user()->id || Auth::user()->is_admin()))
-      <button class="btn" style="float: right"><a href="{{ url('edit/'.$post->slug)}}">Edit Post</a></button>
+    @if($post)
+        {{ $post->title }}
+        @if(!Auth::guest() && ($post->author_id == Auth::user()->id || Auth::user()->is_admin()))
+            <button class="btn" style="float: right"><a href="{{ url('edit/'.$post->slug)}}">Edit Post</a></button>
+        @endif
+    @else
+        Page does not exist
     @endif
-  @else
-    Page does not exist
-  @endif
 @endsection
 @section('title-meta')
-<p>{{ $post->created_at->format('M d,Y \a\t h:i a') }} By <a href="{{ url('/user/'.$post->author_id)}}">{{ $post->author->name }}</a></p>
+    <p>{{ $post->created_at->format('M d,Y \a\t h:i a') }} By <a
+            href="{{ url('/user/'.$post->author_id)}}">{{ $post->author->name }}</a></p>
 @endsection
 @section('content')
-@if($post)
-  <div>
-    {!! $post->body !!}
-  </div>
-@else
-404 error
-@endif
+    @if($post)
+        <section>
+            <h1>{{ $post->title }}</h1>
+            <h2>{{ Auth::user()->fullname }}</h2>
+            <p>{{ $post->body }}</p>
+        </section>
+    @else
+        404 error
+    @endif
 @endsection
