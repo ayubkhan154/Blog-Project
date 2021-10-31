fc1a93024f9abe45d707522c433ab8d709c0a698
Ayub Khan <ayubkhan154@live.com>
2021-10-31 14:12:32 -0400

    updated backend

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -25,13 +25,6 @@
                         @endif
                     </tr>
                     <tr>
-                        <td>Published Posts</td>
-                        <td>{{$posts_active_count}}</td>
-                        @if($posts_active_count)
-                            <td><a href="{{ url('/user/'.$user->id.'/posts')}}">Show All</a></td>
-                        @endif
-                    </tr>
-                    <tr>
                         <td>Posts in Draft </td>
                         <td>{{$posts_draft_count}}</td>
                         @if($posts_draft_count)
@@ -45,10 +38,11 @@
         <br>
         <div class="">
             <div class="list-group">
+                <h2>Published Posts</h2>
                 @foreach( $posts as $post )
 
                     <div class="list-group-item">
-                        <h3><a href="{{ url('/'.$post->slug) }}">{{ $post->title }}</a>
+                        <h3><a href="{{ url('post/'.$post->slug) }}">{{ $post->title }}</a>
                             @if(!Auth::guest() && ($post->author_id == Auth::user()->id || Auth::user()->is_admin()))
                                 @if($post->active == '1')
                                     <button class="btn" style="float: right"><a href="{{ url('edit/'.$post->slug)}}">Edit
