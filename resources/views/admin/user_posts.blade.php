96d48697c93dbfa61841b7c5605249f6132c55ac
Akshit Tripathi <tripathisoft@gmail.com>
2021-10-31 18:21:36 -0400

    Update admin/user_posts styles

--- resources/views/admin/user_posts.blade.php
+++ resources/views/admin/user_posts.blade.php
@@ -3,23 +3,35 @@
     User Posts
 @endsection
 @section('content')
-    <table>
-        <tr>
-            <th>Post Title</th>
-            <th>Published</th>
-            <th>Last Updated</th>
-            <th>Actions</th>
-        </tr>
-        @foreach($posts as $post)
+    <div class="bg-white border">
+        <table class="table m-0">
+            <thead>
             <tr>
-                <td><a href="/post/{{ $post->slug }}">{{ $post->title }}</a></td>
-                <td>{{ $post->created_at->format('M d,Y \a\t h:i a') }}</td>
-                <td>{{ $post->updated_at->format('M d,Y \a\t h:i a') }}</td>
-                <td>
-                    <a href="/edit/{{ $post->slug }}">Edit</a>
-                    <a href="/delete/{{ $post->id }}">Delete</a>
-                </td>
+                <th scope="col">Post Title</th>
+                <th scope="col">Status</th>
+                <th scope="col">Published</th>
+                <th scope="col">Last Updated</th>
+                <th scope="col">Actions</th>
             </tr>
-        @endforeach
-    </table>
+            </thead>
+            @foreach($posts as $post)
+                <tr>
+                    <td><a href="/post/{{ $post->slug }}">{{ $post->title }}</a></td>
+                    <td>
+                        @if($post->active)
+                            <span class="badge badge-success">PUBLISHED</span>
+                        @else
+                            <span class="badge badge-secondary">DRAFT</span>
+                        @endif
+                    </td>
+                    <td>{{ $post->created_at->format('M d,Y \a\t h:i a') }}</td>
+                    <td>{{ $post->updated_at->format('M d,Y \a\t h:i a') }}</td>
+                    <td>
+                        <a class="btn btn-info" href="/edit/{{ $post->slug }}">Edit</a>
+                        <a class="btn btn-danger" href="/delete/{{ $post->id }}">Delete</a>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
 @endsection
